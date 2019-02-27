dd72a2474671f78a265fcf759154f414de2aba3f
DESKTOP-C5D9A1P\Leo <leonardo.badilla2@gmail.com>
2019-02-27 17:45:00 +0100

    react snap comeback

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from "react";
+/*
 import ReactDOM from "react-dom";
-import Nav from "./Nav";
 import App from "./App";
+*/
 import { BrowserRouter } from "react-router-dom";
-
+import React from "react";
+import Nav from "./Nav";
+import { hydrate, render } from "react-dom";
+/*
 ReactDOM.render((
   // <App />,
   <BrowserRouter>
     <Nav />
   </BrowserRouter>),
-  document.getElementById("root"));
+  document.getElementById("root"));*/
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<BrowserRouter><Nav /></BrowserRouter>, rootElement);
+} else {
+  render(<BrowserRouter><Nav /></BrowserRouter>, rootElement);
+}
